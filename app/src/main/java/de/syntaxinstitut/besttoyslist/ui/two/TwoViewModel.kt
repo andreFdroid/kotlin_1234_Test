1c3f8b748947191029c0107d6fa29afa2123deb5
André Wagner <100126751+andreFdroid@users.noreply.github.com>
2022-06-20 12:09:19 +0200

    initial commit

---
+++ app/src/main/java/de/syntaxinstitut/besttoyslist/ui/two/TwoViewModel.kt
@@ -0,0 +1,10 @@
+package de.syntaxinstitut.besttoyslist.ui.two
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+/**
+ * Das ViewModel des One Fragments
+ */
+class TwoViewModel(application: Application) : AndroidViewModel(application) {
+}
